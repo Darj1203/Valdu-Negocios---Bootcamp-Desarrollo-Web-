91648d64d7352a843c37634696c555e61f8cd715
Darj1203 <darj1203@gmail.com>
2023-11-07 17:45:46 -0500

    Proyecto hecho en vite con VUE y código de la clase 2
    Se genera el código de la clase 2

---
+++ landing-cmr/src/App.vue
@@ -0,0 +1,112 @@
+<template>
+    <CabeceraVue />
+    <main class=" ">
+      <section id="presentacion" class="md:flex md:flex-row m-auto bg-gradient-to-r from-blue-500 to-blue-900 md:mt-16 p-16">
+        <!--Presentaremos el CMR donde diremos que es una solución hecha por Valdu Negocios, para todos los negocios de Colombia que requieren bases de datos, seguimiento, automatización de procesos, gestión de ventas, Analisis y reportes-->
+        <div class="mt-10 gap-7 md:p-10 md:w-1/2 text-gray-50">
+          <img class="w-full h-full object-cover rounded-3xl my-auto" src="../public/imagenCRM.webp">
+        </div>
+        <div class="gap-7 md:p-16 md:w-1/2 text-gray-50 my-auto"> 
+          <h2 class="text-center text-2xl font-bold p-5">¡El CMR de Valdu Negocios está listo para ayudar a optimizar tu estrategia empresarial!</h2>
+          <p class="text-md text-center"><strong>CMR - Por Valdu Negocios</strong>, una solución proporcionada por Valdu Negocios, diseñada para satisfacer las necesidades de todos los negocios en Colombia que requieren:</p>
+        </div>
+      </section>
+      <section id="servicios" class="p-10 text-center scroll-m-10">
+        <h2 class="font-bold text-4xl m-4">Una herramienta de CRM que realmente querrás usar</h2>
+        <p class="m-4 p-4 font-sans text-xl">Automatiza el trabajo repetitivo y dedicate a las ventas</p>
+        <a class="bg-blue-700 hover:bg-blue-900 p-4 m-4 rounded-md text-white" href="#contactenos">Empezar Ahora</a>
+      </section>
+      <section class="md:flex justify-center px-8"> 
+        <div class="md:w-1/3 p-4">
+          <article class="text-center items-center p-8 border-cyan-300 border-solid border-spacing-2 border-2 shadow-md shadow-gray-600 rounded-xl m-4">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10 p-2 bg-teal-300 rounded-lg  m-auto">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M3.375 19.5h17.25m-17.25 0a1.125 1.125 0 01-1.125-1.125M3.375 19.5h7.5c.621 0 1.125-.504 1.125-1.125m-9.75 0V5.625m0 12.75v-1.5c0-.621.504-1.125 1.125-1.125m18.375 2.625V5.625m0 12.75c0 .621-.504 1.125-1.125 1.125m1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125m0 3.75h-7.5A1.125 1.125 0 0112 18.375m9.75-12.75c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125m19.5 0v1.5c0 .621-.504 1.125-1.125 1.125M2.25 5.625v1.5c0 .621.504 1.125 1.125 1.125m0 0h17.25m-17.25 0h7.5c.621 0 1.125.504 1.125 1.125M3.375 8.25c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125m17.25-3.75h-7.5c-.621 0-1.125.504-1.125 1.125m8.625-1.125c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h7.5m-7.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125M12 10.875v-1.5m0 1.5c0 .621-.504 1.125-1.125 1.125M12 10.875c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125M13.125 12h7.5m-7.5 0c-.621 0-1.125.504-1.125 1.125M20.625 12c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h7.5M12 14.625v-1.5m0 1.5c0 .621-.504 1.125-1.125 1.125M12 14.625c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125m0 1.5v-1.5m0 0c0-.621.504-1.125 1.125-1.125m0 0h7.5" />
+            </svg>
+            <h3 class="font-bold text-lg p-3">Bases de datos completas</h3>
+            <p>Detalles actualizados para conocer a tus clientes y aumentar ventas.</p>
+          </article>
+          <article class="text-center items-center p-8 border-cyan-300 border-solid border-spacing-2 border-2 shadow-md shadow-gray-600 rounded-xl m-4">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10 p-2 bg-teal-300 rounded-lg  m-auto">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5m.75-9l3-3 2.148 2.148A12.061 12.061 0 0116.5 7.605" />
+            </svg>
+            <h3 class="font-bold text-lg p-3">Automatización de procesos</h3>
+            <p>Ahorra tiempo con tareas automáticas y eleva la productividad</p>
+          </article>
+          <article class="text-center items-center p-8 border-cyan-300 border-solid border-spacing-2 border-2 shadow-md shadow-gray-600 rounded-xl m-4">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" class="w-10 h-10 p-2 bg-teal-300 rounded-lg  m-auto">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+            </svg>
+            <h3 class="font-bold text-lg p-3">Gestión de ventas</h3>
+            <p>Haz crecer tus ventas con seguimiento de oportunidades y pedidos.</p>
+          </article>
+        </div>
+        <div class="md:w-1/3 p-4">
+          <img class="w-full h-full object-cover rounded-3xl" src="../public/imagenCRM.webp">
+        </div>
+        <div class="md:w-1/3 p-4">
+          <article class="text-center items-center p-8 border-cyan-300 border-solid border-spacing-2 border-2 shadow-md shadow-gray-600 rounded-xl m-4">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10 p-2 bg-teal-300 rounded-lg  m-auto">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M3.375 19.5h17.25m-17.25 0a1.125 1.125 0 01-1.125-1.125M3.375 19.5h7.5c.621 0 1.125-.504 1.125-1.125m-9.75 0V5.625m0 12.75v-1.5c0-.621.504-1.125 1.125-1.125m18.375 2.625V5.625m0 12.75c0 .621-.504 1.125-1.125 1.125m1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125m0 3.75h-7.5A1.125 1.125 0 0112 18.375m9.75-12.75c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125m19.5 0v1.5c0 .621-.504 1.125-1.125 1.125M2.25 5.625v1.5c0 .621.504 1.125 1.125 1.125m0 0h17.25m-17.25 0h7.5c.621 0 1.125.504 1.125 1.125M3.375 8.25c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125m17.25-3.75h-7.5c-.621 0-1.125.504-1.125 1.125m8.625-1.125c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h7.5m-7.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125M12 10.875v-1.5m0 1.5c0 .621-.504 1.125-1.125 1.125M12 10.875c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125M13.125 12h7.5m-7.5 0c-.621 0-1.125.504-1.125 1.125M20.625 12c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h7.5M12 14.625v-1.5m0 1.5c0 .621-.504 1.125-1.125 1.125M12 14.625c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125m0 1.5v-1.5m0 0c0-.621.504-1.125 1.125-1.125m0 0h7.5" />
+            </svg>
+            <h3 class="font-bold text-lg p-3">Análisis y reportes</h3>
+            <p>Toma decisiones inteligentes con datos claros sobre ventas y clientes.</p>
+          </article>
+          <article class="text-center items-center p-8 border-cyan-300 border-solid border-spacing-2 border-2 shadow-md shadow-gray-600 rounded-xl m-4">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10 p-2 bg-teal-300 rounded-lg  m-auto">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5m.75-9l3-3 2.148 2.148A12.061 12.061 0 0116.5 7.605" />
+            </svg>
+            <h3 class="font-bold text-lg p-3">Integración con otras herramientas</h3>
+            <p>Conexión total: unifica tus plataformas para un control eficiente.</p>
+          </article>
+          <article class="text-center items-center p-8 border-cyan-300 border-solid border-spacing-2 border-2 shadow-md shadow-gray-600 rounded-xl m-4">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" class="w-10 h-10 p-2 bg-teal-300 rounded-lg  m-auto">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+            </svg>
+            <h3 class="font-bold text-lg p-3">Seguridad y privacidad</h3>
+            <p>Protege la confidencialidad de tus clientes y cumple regulaciones.</p>
+          </article>
+        </div>
+      </section>
+      <section id="contactenos" class="md:flex justify-center bg-blue-500 text-center md:p-16 p-4 items-center scroll-m-10">
+        <div class="md:w-1/2 p-12">
+          <h2 class="text-white text-2xl font-bold mb-4">Descubre cómo un CRM transformará tu empresa</h2>
+          <p class="text-white mb-4">Enfrentar los desafíos empresariales a menudo requiere la herramienta adecuada. ¿Sientes que falta algo en la gestión de tus clientes? Estamos aquí para ayudarte. Cuéntanos qué necesitas. Ya sea el seguimiento eficiente de ventas, una mejor comprensión de tus clientes o la automatización de procesos, juntos encontraremos la solución ideal para potenciar tu empresa.</p>
+          <div class="flex gap-4 justify-center">
+            <a href="https://www.facebook.com/valdunegocios" target="_blank" rel="noopener noreferrer">
+              <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.0" viewBox="0 0 50 50" style="null" class="instagram-logo w-10 h-10 fill-slate-100" >    <path d="M40,0H10C4.486,0,0,4.486,0,10v30c0,5.514,4.486,10,10,10h30c5.514,0,10-4.486,10-10V10C50,4.486,45.514,0,40,0z M39,17h-3 c-2.145,0-3,0.504-3,2v3h6l-1,6h-5v20h-7V28h-3v-6h3v-3c0-4.677,1.581-8,7-8c2.902,0,6,1,6,1V17z"></path></svg>
+            </a>
+            <a href="https://www.instagram.com/valdunegocios" target="_blank" rel="noopener noreferrer">
+              <svg class="instagram-logo w-10 h-10 fill-slate-100" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 551.034 551.034" style="enable-background:new 0 0 000.000 000.000;" xml:space="preserve">
+              <path class="logo  fill-slate-100" id="XMLID_17_" d="M386.878,0H164.156C73.64,0,0,73.64,0,164.156v222.722 c0,90.516,73.64,164.156,164.156,164.156h222.722c90.516,0,164.156-73.64,164.156-164.156V164.156 C551.033,73.64,477.393,0,386.878,0z M495.6,386.878c0,60.045-48.677,108.722-108.722,108.722H164.156 c-60.045,0-108.722-48.677-108.722-108.722V164.156c0-60.046,48.677-108.722,108.722-108.722h222.722 c60.045,0,108.722,48.676,108.722,108.722L495.6,386.878L495.6,386.878z"/>
+              <path id="XMLID_81_" d="M275.517,133C196.933,133,133,196.933,133,275.516 s63.933,142.517,142.517,142.517S418.034,354.1,418.034,275.516S354.101,133,275.517,133z M275.517,362.6 c-48.095,0-87.083-38.988-87.083-87.083s38.989-87.083,87.083-87.083c48.095,0,87.083,38.988,87.083,87.083 C362.6,323.611,323.611,362.6,275.517,362.6z"/>
+              <circle id="XMLID_83_" cx="418.306" cy="134.072" r="34.149"/>
+              </svg>
+            </a>
+          </div>
+        </div>
+        <div class="md:w-1/2 text-left gap-4">
+          <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+              <label class="block text-gray-700 font-bold mb-2" for="nombre">Nombre</label>
+              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="nombre" type="text" placeholder="Nombre">
+              <label class="block text-gray-700 font-bold mb-2" for="correo">Correo</label>
+              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="correo" type="email" placeholder="Correo">
+              <label class="block text-gray-700 font-bold mb-2" for="telefono">Teléfono</label>
+              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="telefono" type="tel" placeholder="Teléfono">
+              <label class="block text-gray-700 font-bold mb-2" for="cmr">¿Cómo desea su CMR personalizado?</label>
+              <textarea class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="cmr" placeholder="Escriba aquí"></textarea>
+              <input type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full" value="Enviar">
+          </form>
+        </div>
+      </section>
+
+    </main>
+</template>
+
+<script setup>
+  import CabeceraVue from './components/Cabecera.vue';
+</script>
+
+<style scoped>
+
+</style>
---
+++ landing-cmr/src/components/Cabecera.vue
@@ -0,0 +1,29 @@
+<template>
+    <!-- Aquí va el contenido del header --> 
+    <header class="fixed top-0 w-full z-50 ">
+    <div hidden>
+      <h1>CMR - Creado por Valdu Negocios</h1>
+    </div>
+    <nav class="nav bg-gray-800">
+      <ul class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 text-gray-300  flex flex-row gap-2 flex-auto justify-center  p-5">
+        <li class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 font-medium ">
+          <a href="#presentacion">Presentación</a>
+        </li>
+        <li class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 font-medium">
+          <a href="#servicios">Servicios</a>
+        </li>
+        <li class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 font-medium ">
+          <a href="#contactenos">Contactanos</a>
+        </li>
+      </ul>
+    </nav>
+    </header>
+</template>
+
+<script>
+
+</script>
+
+<style>
+    
+</style>
